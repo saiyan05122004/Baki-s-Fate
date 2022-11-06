776205271ac9a9692784f5d848f476e773c3ad17
saiyan05122004 <97660578+saiyan05122004@users.noreply.github.com>
2022-11-06 20:25:13 +0300

    Update CameraFollow.cs

--- Scripts/CameraFollow.cs
+++ Scripts/CameraFollow.cs
@@ -1,26 +1,26 @@
 using UnityEngine;
 using System.Collections;
 
-public class CameraFollow : MonoBehaviour
+public class CameraFollow : MonoBehaviour //Нахождение игрока и его отслеживание
 {
-
+    //Добавление переменных
     public float damping = 1.5f;
     public Vector2 offset = new Vector2(2f, 1f);
     public bool faceLeft;
     private Transform player;
     private int lastX;
 
-    void Start()
+    void Start() // Начальные действия кода (Найти игрока , по ссылке на публичный класс)
     {
-        offset = new Vector2(Mathf.Abs(offset.x), offset.y);
-        FindPlayer(faceLeft);
+        offset = new Vector2(Mathf.Abs(offset.x), offset.y); // Вычисление математической формулой
+        FindPlayer(faceLeft); // ссылка на FindPlayer
     }
 
-    public void FindPlayer(bool playerFaceLeft)
+    public void FindPlayer(bool playerFaceLeft) // Публичный класс с добавлением переменной
     {
-        player = GameObject.FindGameObjectWithTag("Player").transform;
+        player = GameObject.FindGameObjectWithTag("Player").transform; // Находим игрока по добавленному ему тэгу
         lastX = Mathf.RoundToInt(player.position.x);
-        if (playerFaceLeft)
+        if (playerFaceLeft) //Создаём условие если игрок изначально смотрит налево
         {
             transform.position = new Vector3(player.position.x - offset.x, player.position.y + offset.y, transform.position.z);
         }
@@ -30,9 +30,9 @@ public class CameraFollow : MonoBehaviour
         }
     }
 
-    void Update()
+    void Update() // Постоянное отслеживание игрока и обновление данных
     {
-        if (player)
+        if (player) // Условие с мат. вычислениями
         {
             int currentX = Mathf.RoundToInt(player.position.x);
             if (currentX > lastX) faceLeft = false; else if (currentX < lastX) faceLeft = true;
